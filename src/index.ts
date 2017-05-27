ad509ab106dc3082e78677d58c51a85c3cc27322
Mitch Crowe <crowe.mitch@gmail.com>
2017-05-27 10:45:54 -0700

    Initial version.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,35 @@
-function doSomething(a: number): number {
-  return a * 5
+function pull<T>(xs: T[], x: T) {
+  const idx = xs.indexOf(x)
+  if (idx > -1) {
+    xs.splice(idx, 1)
+  }
 }
 
 
-export default doSomething
+class Channel {
+
+  listeners: Function[]
+
+  constructor() {
+    this.listeners = []
+  }
+
+  subscribe(fn: Function) {
+    this.listeners.push(fn)
+
+    // Return an unsubscribe function.
+    return () => {
+      pull(this.listeners, fn)
+    }
+  }
+
+  broadcast(val?) {
+    this.listeners.forEach(fn => {
+      fn(val)
+    })
+  }
+
+}
+
+
+export default Channel
